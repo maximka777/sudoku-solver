b1a85444d4bec2b6b777e6dcbf8186bf79e7a268
maximka777 <maksim.borovskij@yandex.ru>
2021-07-11 11:17:42 +0200

    ** solver

--- src/solver/index.js
+++ src/solver/index.js
@@ -1,5 +1,120 @@
-export class Solver {
-    solve() {
-        return 'Solved';
+const SUDOKU_MAGIC_NUMBER = 9;
+
+const SQUARE_NUMBER_BY_COORDINATES = [
+    [1, 1, 1, 2, 2, 2, 3, 3, 3],
+    [1, 1, 1, 2, 2, 2, 3, 3, 3],
+    [1, 1, 1, 2, 2, 2, 3, 3, 3],
+    [4, 4, 4, 5, 5, 5, 6, 6, 6],
+    [4, 4, 4, 5, 5, 5, 6, 6, 6],
+    [4, 4, 4, 5, 5, 5, 6, 6, 6],
+    [7, 7, 7, 8, 8, 8, 9, 9, 9],
+    [7, 7, 7, 8, 8, 8, 9, 9, 9],
+    [7, 7, 7, 8, 8, 8, 9, 9, 9]
+];
+
+// sudoku is a matrix 9*9 (array of nine arrays of nine digits)
+// null means empty cell
+// digit means filled in cell
+export function solve(sudoku) {
+    let internalSudoku = initialSudokuToInternalRepresentation(sudoku);
+    console.log(internalSudokuToString(internalSudoku));
+    internalSudoku = calculatePotentialSolutions(internalSudoku);
+    console.log(internalSudokuToString(internalSudoku));
+    let previousNumberOfUnsolvedCells = getNumberOfUnsolvedCells(internalSudoku);
+    while (true) {
+        internalSudoku = calculatePotentialSolutions(internalSudoku);
+        console.log(internalSudokuToString(internalSudoku));
+        const currentNumberOfUnsolvedCells = getNumberOfUnsolvedCells(internalSudoku);
+        if (currentNumberOfUnsolvedCells === previousNumberOfUnsolvedCells) {
+            throw new Error('Can\'t solve sudoku :(');
+        }
+        if (currentNumberOfUnsolvedCells === 0) {
+            break;
+        }
+        previousNumberOfUnsolvedCells = currentNumberOfUnsolvedCells;
     }
+    return internalSudokuToExternalRepresentation(internalSudoku); 
+}
+
+export function initialSudokuToInternalRepresentation(sudoku) {
+    return sudoku.reduce((result, line) => {
+        return [...result, line.map(v => {
+            if (v) {
+                return [v];
+            }
+            return [];
+        })];
+    }, []);
+}
+
+export function internalSudokuToExternalRepresentation(sudoku) {
+    return sudoku.reduce((result, line) => {
+        return [...result, line.map(v => v[0] || null)];
+    }, []);
+}
+
+export function calculatePotentialSolutions(internalSudoku) {
+    return internalSudoku.reduce((result, line, i) => {
+        return [...result, line.map((v, j) => {
+            if (v.length !== 1) {
+                return calculatePotentialSolutionsForCell(internalSudoku, i, j);
+            }
+            return v;
+        })];
+    }, []);
+}
+
+// i - row
+// j - column
+export function calculatePotentialSolutionsForCell(internalSudoku, i, j) {
+    const rowDigits = getRowDigits(internalSudoku, i);
+    const columnDigits = getColumnDigits(internalSudoku, j);
+    const squareDigits = getSquareDigits(internalSudoku, getSquareNumberByCellCoordinates(i, j));
+    return [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    .filter(v => !rowDigits.includes(v))
+    .filter(v => !columnDigits.includes(v))
+    .filter(v => !squareDigits.includes(v));
+}
+
+export function getRowDigits(internalSudoku, r) {
+    return internalSudoku[r].filter(v => v.length === 1).map(v => v[0]);
+}
+
+export function getColumnDigits(internalSudoku, c) {
+    const columnDigits = [];
+    for (let i = 0; i < SUDOKU_MAGIC_NUMBER; i++) {
+        const cell = internalSudoku[i][c];
+        if (cell.length === 1) {
+            columnDigits.push(cell[0]);
+        }
+    }
+    return columnDigits;
+}
+
+// square is a matrix 3*3 and sudoku is made of 9 such squares
+export const getSquareNumberByCellCoordinates = (i, j) => SQUARE_NUMBER_BY_COORDINATES[i][j];
+
+export function getSquareDigits(internalSudoku, squareNumber) {
+    const squareDigits = [];
+    for (let i = 0; i < SUDOKU_MAGIC_NUMBER; i++) {
+        for (let j = 0; j < SUDOKU_MAGIC_NUMBER; j++) {
+            if (SQUARE_NUMBER_BY_COORDINATES[i][j] === squareNumber) {
+                const cell = internalSudoku[i][j];
+                if (cell.length === 1) {
+                    squareDigits.push(cell[0]);
+                }
+            }
+        }
+    }
+    return squareDigits;
+}
+
+export function getNumberOfUnsolvedCells(internalSudoku) {
+    return internalSudoku.reduce((acc, line) => {
+        return acc + line.filter(v => v.length !== 1).length;
+    }, 0);
+}
+
+export function internalSudokuToString(internalSudoku) {
+    return internalSudoku.map(row => row.map(cell => '[' + cell.join('') + ']').join(', ')).join('\n');
 }
--- tests/solver/index.test.js
+++ tests/solver/index.test.js
@@ -1,7 +1,82 @@
-import { Solver } from '../../src/solver';
+import { solve, getSquareNumberByCellCoordinates, getSquareDigits, initialSudokuToInternalRepresentation, calculatePotentialSolutionsForCell, getRowDigits, getColumnDigits, getNumberOfUnsolvedCells, internalSudokuToString } from '../../src/solver';
 
-describe('Solver', () => {
-    it('solves correectly', () => {
-        expect(new Solver().solve()).toBe('Solved');
+const TEST_INITIAL_SUDOKU = [
+    [3, null, 7, null, 8, 1, null, null, null],
+    [null, null, null, 5, null, null, 3, 8, null],
+    [null, null, null, 7, null, null, 1, 6, null],
+    [null, 8, 5, null, null, null, null, 4, 2],
+    [4, null, null, null, null, null, null, null, 5],
+    [6, 9, null, null, null, null, 7, 3, null],
+    [null, 2, 3, null, null, 7, null, null, null],
+    [null, 6, 8, null, null, 9, null, null, null],
+    [null, null, null, 1, 6, null, 2, null, 3]
+];
+
+describe('solver', () => {
+    it('solves correctly', () => {
+        const solvedSudoku = [
+            [3, 4, 7, 6, 8, 1, 5, 2, 9],
+            [2, 1, 6, 5, 9, 4, 3, 8, 7],
+            [8, 5, 9, 7, 3, 2, 1, 6, 4],
+            [7, 8, 5, 9, 1, 3, 6, 4, 2],
+            [4, 3, 2, 8, 7, 6, 9, 1, 5],
+            [6, 9, 1, 2, 4, 5, 7, 3, 8],
+            [1, 2, 3, 4, 5, 7, 8, 9, 6],
+            [5, 6, 8, 3, 2, 9, 4, 7, 1],
+            [9, 7, 4, 1, 6, 8, 2, 5, 3]
+        ];
+        expect(solve(TEST_INITIAL_SUDOKU).toString()).toBe(solvedSudoku.toString());
+    });
+
+    it('returns correct square number by cell coordinates', () => {
+        // 1
+        expect(getSquareNumberByCellCoordinates(0, 0)).toBe(1);
+        expect(getSquareNumberByCellCoordinates(0, 1)).toBe(1);
+        expect(getSquareNumberByCellCoordinates(0, 2)).toBe(1);
+        expect(getSquareNumberByCellCoordinates(1, 0)).toBe(1);
+        expect(getSquareNumberByCellCoordinates(1, 1)).toBe(1);
+        expect(getSquareNumberByCellCoordinates(1, 2)).toBe(1);
+        expect(getSquareNumberByCellCoordinates(2, 0)).toBe(1);
+        expect(getSquareNumberByCellCoordinates(2, 1)).toBe(1);
+        expect(getSquareNumberByCellCoordinates(2, 2)).toBe(1);
+        // 4
+        expect(getSquareNumberByCellCoordinates(3, 0)).toBe(4);
+        expect(getSquareNumberByCellCoordinates(3, 1)).toBe(4);
+        expect(getSquareNumberByCellCoordinates(3, 2)).toBe(4);
+        expect(getSquareNumberByCellCoordinates(4, 0)).toBe(4);
+        expect(getSquareNumberByCellCoordinates(4, 1)).toBe(4);
+        expect(getSquareNumberByCellCoordinates(4, 2)).toBe(4);
+        expect(getSquareNumberByCellCoordinates(5, 0)).toBe(4);
+        expect(getSquareNumberByCellCoordinates(5, 1)).toBe(4);
+        expect(getSquareNumberByCellCoordinates(5, 2)).toBe(4);
+    });
+
+    it('returns correct square digits by square number', () => {
+        expect(getSquareDigits(initialSudokuToInternalRepresentation(TEST_INITIAL_SUDOKU), 1).toString()).toBe([3, 7].toString());
+        expect(getSquareDigits(initialSudokuToInternalRepresentation(TEST_INITIAL_SUDOKU), 7).toString()).toBe([2, 3, 6, 8].toString());
+    });
+
+    it('calculates correct potential solutions for cell', () => {
+        expect(calculatePotentialSolutionsForCell(initialSudokuToInternalRepresentation(TEST_INITIAL_SUDOKU), 0, 1).toString()).toBe([4, 5].toString());
+    });
+
+    it('returns correct row digits', () => {
+        expect(getRowDigits(initialSudokuToInternalRepresentation(TEST_INITIAL_SUDOKU), 1).toString()).toBe([5, 3, 8].toString());
+    });
+
+    it('returns correct column digits', () => {
+        expect(getColumnDigits(initialSudokuToInternalRepresentation(TEST_INITIAL_SUDOKU), 1).toString()).toBe([8, 9, 2, 6].toString());
+    });
+
+    it('returns correct number of unsolved cells', () => {
+        expect(getNumberOfUnsolvedCells(initialSudokuToInternalRepresentation(TEST_INITIAL_SUDOKU))).toBe(51);
+    });
+
+    it('converts mini-sudoku to correct string', () => {
+        expect(internalSudokuToString([
+            [[1,2,3], [5], [7]],
+            [[], [6,7], [8,9]],
+            [[], [], []]
+        ])).toBe('[123], [5], [7]\n[], [67], [89]\n[], [], []')
     });
 });
